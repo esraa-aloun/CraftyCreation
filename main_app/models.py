3fb8d1b36cfbc26fdb4ab8cc3c8dd94acb66815c
esraa-aloun <aloun.esraa@gmail.com>
2023-03-18 13:10:03 +0300

    profile model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+ 
 # Create your models here.
+class Profile(AbstractUser):
+    userRole = models.CharField(max_length=30, required= True),
+    gender = models.CharField(max_length=30, required= True),
+    age = models.IntegerField(max_length=30, required= True),
+    category = models.CharField(max_length=60, default = 'default'),
+    certification = models.FileField(max_length=60, default ='default_file.txt')
