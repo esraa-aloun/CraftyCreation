573ceddef78e5077b6b5ba59617c885ebfc3d9b5
esraa-aloun <aloun.esraa@gmail.com>
2023-03-18 12:42:32 +0300

    raw code

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
+# Create your views here.
+def home(request):   
+    return render(request, 'home.html')
